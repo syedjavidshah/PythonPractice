f3030853065436414ad935217b2240ffb968312e
syed javid shah <syedjavid204@gmail.com>
2023-12-13 16:56:38 +0530

    This is Strings code

---
+++ tut2.py
@@ -0,0 +1,12 @@
+ystr = "syed is a good boy"
+#print(len(mystr))
+#print(mystr[14:16])
+
+print (mystr.endswith("boy"))
+print(mystr.count("o"))
+print(mystr.capitalize())
+print((mystr.find("is")))
+print(mystr.lower())
+print(mystr.upper())
+print(mystr.replace("is","are"))
+# print(mystr.replace("syed","javid"))
