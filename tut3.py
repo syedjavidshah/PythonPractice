e123451ad3929025e945a380ac335fd730c1f2d7
syed javid shah <syedjavid204@gmail.com>
2023-12-13 17:02:56 +0530

    This is Dictionary  code

---
+++ tut3.py
@@ -0,0 +1,68 @@
+#dictionary is key value pairs
+#d1= {}
+#print(type(d1))
+d2 = {"javid":"meat","abc" : "chicken","ali":"roti"}
+print(d2)
+#print(d2["javid"])
+#print(d2["ali"])
+#d2["syed"]="junk food"
+#d2["aliyas"]="cold drink"
+#del d2["aliyas"]
+#print(d2)
+#print(d2.copy())
+print(d2.get("abc"))
+print(d2.update({"syed":"ice cream"}))
+print(d2)
+print(d2.keys())
+print(d2.items())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
