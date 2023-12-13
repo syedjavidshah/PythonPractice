2331fa7425c6b1338dcb3bba206f15b43d063e85
syed javid shah <106911386+syedjavidshah@users.noreply.github.com>
2023-12-13 17:08:14 +0530

    Update tut3.py

--- tut3.py
+++ tut3.py
@@ -1,15 +1,15 @@
-#dictionary is key value pairs
-#d1= {}
-#print(type(d1))
+# dictionary is key value pairs
+d1= {}
+print(type(d1))
 d2 = {"javid":"meat","abc" : "chicken","ali":"roti"}
 print(d2)
-#print(d2["javid"])
-#print(d2["ali"])
-#d2["syed"]="junk food"
-#d2["aliyas"]="cold drink"
-#del d2["aliyas"]
-#print(d2)
-#print(d2.copy())
+print(d2["javid"])
+print(d2["ali"])
+d2["syed"]="junk food"
+d2["aliyas"]="cold drink"
+del d2["aliyas"]
+print(d2)
+print(d2.copy())
 print(d2.get("abc"))
 print(d2.update({"syed":"ice cream"}))
 print(d2)
