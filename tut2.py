42a56a5ab85d79608dd8005e87e172112683483f
syed javid shah <106911386+syedjavidshah@users.noreply.github.com>
2023-12-13 17:08:34 +0530

    Update tut2.py

--- tut2.py
+++ tut2.py
@@ -1,6 +1,6 @@
 ystr = "syed is a good boy"
-#print(len(mystr))
-#print(mystr[14:16])
+print(len(mystr))
+print(mystr[14:16])
 
 print (mystr.endswith("boy"))
 print(mystr.count("o"))
@@ -9,4 +9,4 @@ print((mystr.find("is")))
 print(mystr.lower())
 print(mystr.upper())
 print(mystr.replace("is","are"))
-# print(mystr.replace("syed","javid"))
+ print(mystr.replace("syed","javid"))
